2bdee5b79a24bf1286cd3ed7d6d40aabfd661f04
vithyaCodegama <vithya@codegama.com>
2020-11-12 17:31:46 +0530

    Social logins + Static pages completed

--- src/components/StaticPage/StaticPage.jsx
+++ src/components/StaticPage/StaticPage.jsx
@@ -1,103 +1,79 @@
-import React from "react";
-import { Container, Row, Col, Image } from "react-bootstrap";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
 import "./StaticPage.css";
+import { connect } from "react-redux";
+import { fetchSinglePageStart } from "../../store/actions/PageAction";
 
-const StaticPage = () => {
+class StaticPage extends Component {
+  state = {
+    pageData: null,
+    loadingPageData: true,
+    active: null,
+    displayContent: null,
+    currentPageTitle: null,
+  };
+  componentDidMount() {
+    this.setState({ currentPageTitle: this.props.match.params.title });
+    setTimeout(() => {
+      this.props.dispatch(
+        fetchSinglePageStart({ unique_id: this.props.match.params.title })
+      );
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.match.params.title !== prevProps.match.params.title) {
+      // call the fetch function again
+      this.props.dispatch(
+        fetchSinglePageStart({ unique_id: this.props.match.params.title })
+      );
+    }
+  }
+
+  changePage = (event, page) => {
+    event.preventDefault();
+    this.setState({ displayContent: page });
+  };
+  render() {
+    const displayContent = this.props.page.pageData;
+
     return (
-        <>
-            <div className="static-page-sec">
-                <Container>
-                    <h4 className="head-title">Privacy Policy</h4>
-                    <Row>
-                        <Col sm="12" md="12">
-                            <div className="static-box">
-                                <h2>Privacy Policy</h2>        
-                                <p>Last updated February 2015.
-                                Welcome to DeViine.com (the "Site"), provided by DeViine LLC, and its affiliates, subsidiaries, parent company and other related companies ("Us", "We", or "DeViine"). This DeViine.com Privacy Policy ("Policy") describes the information we gather from you when you use the Site, mobile applications, and related services (together, the "Services") and how we use, process, and disclose that information. We may add to this Policy with other notices. We may also post additional privacy statements for some portions of the Services. By submitting personal information through our Services, you expressly consent to the transfer of your personal data to our servers in the U.S. for our collection, use, and disclosure in accordance with this Policy.
-   	                            </p>
-                                <h2>Information We Collect</h2>
-                                <p>
-                                    When you create an account and use our Service, we collect the following types of information from you:</p>
-                                   <p> 1. Name;</p>
-                                   <p>2. Contact information such as your email address and phone number;</p>
-                                    <p>3. Demographics information such as your gender and location;</p>
-                                    <p>4. Any other information, such as reviews, content, and bio, you provide us.</p>
-                                    <p>If you create an account using, or otherwise connect your account to, 
-                                        a social networking services account (e.g. Twitter or Facebook), 
-                                        we may also collect information provided to us by such social networking service 
-                                        including for example your: name; email address; birthday; geographic location; interests; 
-                                        profile picture; gender; networks; user ID; list of friends; and any information you have made 
-                                        public on such social networking account.
-                                    We automatically collect information about how you use our services, for example, 
-                                    pages you have viewed. We also collect certain technical information about your device including your Internet protocol address, geo-location information, your browser type, language and identifying information, your operating system and application version, device types, device model and manufacturer, device identifiers, and your device operating system type and version.
-                                    We also use cookies, Web beacons, and URL information to gather information regarding the date and 
-                                    time of your visit and the information for which you searched and which you viewed. Cookies are small 
-                                    pieces of information that a website sends to your computer's hard drive while you are viewing a website. 
-                                    We may use both session cookies (which expire once you close your web browser) and persistent cookies 
-                                    (which usually stay on your computer until you delete them) to provide you with a more personal and 
-                                    interactive experience on our Site. Web beacons are digital images that are used to log information on 
-                                    the Services or in our emails. We use Web beacons to manage cookies, count visits, and to learn what 
-                                    marketing works and what does not. We also use Web beacons to tell if you open or act on our emails.
-                                    Others, including third party analytics service providers and advertising partners may also 
-                                    collect personally identifiable information about your online activities over time and 
-                                    across different Web sites when you use our Services, including as described in this Privacy 
-                                    Policy. This Policy does not apply to and we are not responsible for those other parties. 
-                                    Third party analytics services may use cookies and web beacons through our Site 
-                                    (for example Google Analytics) to provide us with information about how you use and interact 
-                                    with our Site. We encourage you to review the privacy policies of these third parties to 
-                                    learn about your choices about information they collect from you.</p>
-                                <h2>Sharing Of Your Information</h2>
-                                <p>	The DeViine Services may allow you to connect and share your actions, comments, content, profile, and information publicly or with other people. Please be mindful of your own privacy needs as you choose who to connect with and what to share and make public. We cannot control the privacy or security of information you choose to make public or share with others.
-                                The Services may also provide you with the option to share certain information from your DeViine account with social networking services like Facebook, Google, or Twitter. We are not responsible for the use or re-sharing by others of any of your information once it is made public. If you do not want your information to be made public, you should not use the DeViine Services in this manner and/or you should adjust your privacy settings on the applicable social networking service accordingly. We are not responsible for and we do not control these social networking services privacy practices. Please review the applicable privacy policy for information about how they use your information.
-	                            </p>
-                                <h2>How we Use Your Information</h2>
-                                <p>
-                                    We generally use your information to:
-                                    Facilitate the creation of and secure your account on the Services;
-                                    Identify you as a user in our system;
-                                    Provide, personalize, and improve the Services;
-                                    Communicate with you about your use of the Services;
-                                    Develop new products and services,
-                                    Customize the advertising you view and recommend content;
-                                    Fulfill your requests;
-                                    Send newsletters, surveys, offers and promotional materials related to the Services and for other marketing purposes of DeViine using your contact information;
-                                    Protect, investigate, and prevent potentially fraudulent, unauthorized, or illegal activities;
-                                    Protect our rights and the rights of other users; and
-                                    As otherwise described in this Privacy Policy or in notices we provided to you.
-                                    We may also use your information to verify your geographic location. We may use your geographic location data to personalize our Service, to recommend content, determine whether the information you have requested is available in your location.
-                                </p>
-                                <h2>How we Disclose Your Information</h2>
-                                <p>
-                                    We may share your information as follows:
-                                </p>
-                                    <p>1. We may share your personal information with your consent or at your direction.</p>
-                                    <p>2. We may also share your information with others who perform services on our behalf.</p>
-                                    <p>3. We may disclose your information if we believe we are required to do so by law, or to 
-                                        comply with a court order, judicial or other government subpoena, or warrant.</p>
-                                    <p>4. We also may disclose your information if we believe doing so is appropriate or necessary to prevent any 
-                                    liability, or fraudulent, abusive, or unlawful uses or to protect DeViine and our Services; or any rights, property, 
-                                    or personal safety of DeViine or others.</p>
-                                   <p> 5. In the event that DeViine is or may be acquired by or merged with another company or involved in any other business deal 
-                                       (or negotiation of a business deal) involving sale or transfer of all or part of our business or assets, 
-                                       we may transfer or assign your information as part of or in connection with the transaction. 
-                                       Finally, in the event of insolvency, bankruptcy, or receivership, information may be transferred as 
-                                       a business asset.
-                                    We may also share aggregated and anonymized data with our partners, advertisers, and other third parties.
-                                </p>
-                                <h2>Security</h2>
-                                <p>DeViine takes commercially reasonable steps to help protect your Information against loss, misuse and unauthorized access, or disclosure. No company can fully prevent security risks, however. While we strive to protect your personal information, we cannot guarantee its absolute security. To help protect yourself and your information, choose a unique password for our Services and do not use a password on our Services that you would use on any other website or online service.</p>
-                                <h2>Dispute Resolution</h2>
-                                <p>If you believe that DeViine has not adhered to this Statement, please contact DeViine by e-mail at support@deviine.com. We will do our best to address your concerns. If you feel that your complaint has been addressed incompletely, we invite you to let us know for further investigation.</p>
-                                <h2>Information Choices</h2>
-                                <p>You may opt out of receiving promotional emails from DeViine by following the instructions in those emails. If you opt out, we may still send you non-promotional emails, such as emails about your accounts or our ongoing business relations. You may also send requests about your personal information, including changes to your contact preferences, changes to or deletions of your information or content you post, and requests to opt-out of sharing your personal information with third parties by emailing support@deviine.com. Please note that deletion of your information or content does not ensure complete or comprehensive removal of the content or information posted on the Services.
-                            </p>
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-        </>
+      <>
+        <div className="static-page-sec">
+          {displayContent.loading ? (
+            "Loading..."
+          ) : (
+            <Container>
+              <h4 className="head-title">{displayContent.data.title}</h4>
+              <Row>
+                <Col sm="12" md="12">
+                  <div className="static-box">
+                    <h5 className="text-muted">
+                      Updated At:{displayContent.data.updated_at}
+                    </h5>
+                    <p
+                      dangerouslySetInnerHTML={{
+                        __html: displayContent.data.description,
+                      }}
+                    ></p>
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          )}
+        </div>
+      </>
     );
-};
+  }
+}
 
-export default StaticPage;
+const mapStateToPros = (state) => ({
+  page: state.page,
+});
+
+function mapDispatchToProps(dispatch) {
+  return { dispatch };
+}
+
+export default connect(mapStateToPros, mapDispatchToProps)(StaticPage);
